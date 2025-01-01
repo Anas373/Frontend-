7c9111fbc14244b61f41e87cc1137fb794709cd5
Anasich <146490943+Anas373@users.noreply.github.com>
2025-01-01 15:34:28 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,42 @@
-import Header from  "./Component/Header"
-import Footer  from "./Component/Footer"
-import Homme  from "./Component/Homme" 
-// import  Herrosection  from "./Component/Herrosection"
-import Specialiter from "./Component/Specialiter"
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
+import React, { useState, useEffect, useCallback } from 'react';
+import Header from './Component/Header';
+import Footer from './Component/Footer';
+import Homme from './Component/Homme';
+import Contact from './Component/Contactus';
+import Specialiter from './Component/Specialiter';
+import AboutUs from './Component/AboutUs';
 
 function App() {
-    return (
-      <>
-        <Header />
+  const [width, setWidth] = useState(window.innerWidth);
+  
+  const handleResize = useCallback(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [handleResize]);
+
+  return (
+    <div
+      className={`container mx-auto p-4 flex flex-col ${
+        width < 768 ? 'md:flex-col' : 'lg:flex-row'
+      }`}
+    >
+      <Header />
+      <main className="flex-grow">
         <Homme />
-       
-          <Specialiter />
-      
-       
-       
-        <Footer />
-      </>
-    );
-  }
-  
-  export default App;
-  
+        <br className="lg:hidden" />
+        <Specialiter />
+        <br className="lg:hidden" />
+        <br className="lg:hidden" />
+        <AboutUs />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
+}
 
-
+export default App;
